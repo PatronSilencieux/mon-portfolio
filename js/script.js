5a14b1920b06b0912fa96fbfe2a0b8a2fdf945fa
JDevInfo <didierzalema56@gmail.com>
2026-08-14 17:43:46 +0100

    Refactor dark mode toggle and related features

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const html = document.documentElement;
 const icon = themeToggle.querySelector("i");
 
 // Vérifier le thème sauvegardé
-const savedTheme = localStorage.getItem("theme") || "light";
+const savedTheme = localStorage.getItem("theme") || "dark";
 html.setAttribute("data-theme", savedTheme);
 updateIcon(savedTheme);
 
